ef87fbde9eb2b88e80ba3727786b5e062701b861
阿部直也 <n.abe@gemcook.com>
2020-11-08 17:50:34 +0900

    counterSliceを削除

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import taskReducer from '../features/task/taskSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     task: taskReducer,
   },
 });
