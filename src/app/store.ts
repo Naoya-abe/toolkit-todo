dab009d2495e24b8ab83312a1fadf9ac780ad743
阿部直也 <n.abe@gemcook.com>
2020-11-08 18:25:56 +0900

    コメントの追加

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
   },
 });
 
+// コンポーネント側でstate参照する時に、型注釈に使用するのでexport
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
